65bd4178f4d9c1c4eff7e67df9a8aa3c9aa316ba
Matthew Huntington Murphy <huntington67@aim.com>
2021-12-28 06:49:51 +0000

    Use @prisma/client.

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -2,7 +2,6 @@
     import { enhance } from "$lib/actions/form";
 
     export let todo: Todo;
-    const done = todo.done
 
     export let processDeletedTodoResult: (res: Response) => void;
     export let processUpdatedTodoResult: (res: Response) => void;
@@ -87,7 +86,7 @@
 
 </style>
 
-<div class="todo" class:done>
+<div class="todo" class:done={todo.done}>
     <form action="/todos/{todo.uid}.json?_method=patch" method="post" use:enhance={{
         result: processUpdatedTodoResult
     }}>
